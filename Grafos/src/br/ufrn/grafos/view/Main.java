f78feefa4343638a9ddd1e6a7c135f4a1663b28f
Alison <alison.jhonas15@gmail.com>
2016-05-27 14:34:23 -0300

    Configuração inicial

---
+++ Grafos/src/br/ufrn/grafos/view/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufrn.grafos.view;
+
+/**
+ *
+ * @author aliso
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GameScreen game = new GameScreen();
+        game.setVisible(true);
+    }
+    
+}
